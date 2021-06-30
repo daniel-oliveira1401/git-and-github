e7e35da1c2d3980b6d3a8296415d6e9a8e37fb75
Daniel Matheus <daniel.oliveira1401@outlook.com>
2021-06-30 08:17:26 -0300

    deleted main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-console.log(
-	"the initial content :). There's an emergency there, but it is all g now :). Also added the new feature btw"
-);
-
-console.log("This is an emergency!!!");
-
-console.log("This is my new awesome feature");
