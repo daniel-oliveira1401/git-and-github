b3134915ff9ffadba266c5e5dfcfc781a77f0905
Daniel Matheus <daniel.oliveira1401@outlook.com>
2021-06-30 08:46:02 -0300

    modified main.js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ console.log(
 	"the initial content :). There's an emergency there, but it is all g now :). Also added the new feature btw"
 );
 
-console.log("This is an emergency!!!");
-
 console.log("This is my new awesome feature");
 
 console.log("Hi, good looking 🙊");
