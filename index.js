47ac589a8f4b21b7c6a6e1ffa5c2894a47ad62dd
daniel-oliveira1401 <61095688+daniel-oliveira1401@users.noreply.github.com>
2021-07-05 08:48:46 -0300

    added the change to test the pull command

--- index.js
+++ index.js
@@ -11,3 +11,5 @@ console.log("Oh hello, im the mod1 branch modification :D");
 console.log("Hi, im learning how to integrate git and github :D");
 
 console.log("Helloo, im the change coming from github editor :D");
+
+console.log("Im the change thats gonna come through the pull command:P");
